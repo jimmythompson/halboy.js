ffa9e246cecf359bb1ecc633faccc72fa0821020
Jimmy Thompson <jimmy.thompson@thoughtworks.com>
2017-08-06 13:49:33 +0100

    Added support for links

--- test/resource.spec.js
+++ test/resource.spec.js
@@ -1,7 +1,31 @@
 import { expect } from 'chai'
 
-describe('resource', () => {
-  it('works lol', () => {
-    expect(true).to.equal(true)
+class HalResource {
+  constructor () {
+    this.links = {}
+  }
+
+  addLink (rel, value) {
+    this.links = {
+      ...this.links,
+      [rel]: value
+    }
+
+    return this
+  }
+
+  getLink (rel) {
+    return this.links[rel]
+  }
+}
+
+describe('HalResource', () => {
+  it('should be able to add and fetch links', () => {
+    const resource =
+      new HalResource()
+        .addLink('self', { href: '/orders' })
+
+    expect(resource.getLink('self'))
+      .to.deep.equal({ href: '/orders' })
   })
 })
