f779669dcdae3e3db113311540d6ea8696971744
Jimmy Thompson <jimmy.thompson@thoughtworks.com>
2017-08-06 14:15:19 +0100

    Added link stacking and embedded resources

---
+++ src/Resource.js
@@ -0,0 +1,45 @@
+const flatten = (arr) =>
+  arr.reduce((result, next) =>
+    result.concat(Array.isArray(next)
+      ? flatten(next)
+      : next), [])
+
+const createOrAppend = (left, right) =>
+  left ? flatten([left, right]) : right
+
+export default class Resource {
+  constructor () {
+    this.links = {}
+    this.embedded = {}
+  }
+
+  getLink (rel) {
+    return this.links[rel]
+  }
+
+  getHref (rel) {
+    return this.getLink(rel).href
+  }
+
+  getResource (key) {
+    return this.embedded[key]
+  }
+
+  addLink (rel, value) {
+    this.links = {
+      ...this.links,
+      [rel]: createOrAppend(this.links[rel], value)
+    }
+
+    return this
+  }
+
+  addResource (rel, value) {
+    this.embedded = {
+      ...this.embedded,
+      [rel]: value
+    }
+
+    return this
+  }
+}
--- test/resource.spec.js
+++ test/resource.spec.js
@@ -1,31 +1,50 @@
 import { expect } from 'chai'
+import Resource from '../src/Resource'
 
-class HalResource {
-  constructor () {
-    this.links = {}
-  }
-
-  addLink (rel, value) {
-    this.links = {
-      ...this.links,
-      [rel]: value
-    }
-
-    return this
-  }
-
-  getLink (rel) {
-    return this.links[rel]
-  }
-}
-
-describe('HalResource', () => {
-  it('should be able to add and fetch links', () => {
+describe('Resource', () => {
+  it('should add and fetch links', () => {
     const resource =
-      new HalResource()
-        .addLink('self', { href: '/orders' })
+      new Resource()
+        .addLink('self', {href: '/orders'})
 
     expect(resource.getLink('self'))
-      .to.deep.equal({ href: '/orders' })
+      .to.deep.equal({href: '/orders'})
+  })
+
+  it('should stack links with the same key', () => {
+    const resource =
+      new Resource()
+        .addLink('ea:admin', {href: '/admins/2', title: 'Fred'})
+        .addLink('ea:admin', {href: '/admins/5', title: 'Kate'})
+
+    expect(resource.getLink('ea:admin'))
+      .to.deep.equal([
+        {href: '/admins/2', title: 'Fred'},
+        {href: '/admins/5', title: 'Kate'}
+      ])
+  })
+
+  it('should fetch hrefs', () => {
+    const resource =
+      new Resource()
+        .addLink('self', {href: '/orders'})
+
+    expect(resource.getHref('self'))
+      .to.deep.equal('/orders')
+  })
+
+  it('should add and fetch embedded resources', () => {
+    const embeddedResource =
+      new Resource()
+        .addLink('self', {href: '/orders/123'})
+        .addLink('ea:basket', {href: '/baskets/98712'})
+        .addLink('ea:customer', {href: '/customers/7809'})
+
+    const resource =
+      new Resource()
+        .addResource('ea:order', embeddedResource)
+
+    expect(resource.getResource('ea:order'))
+      .to.deep.equal(embeddedResource)
   })
 })
