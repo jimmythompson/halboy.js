68b32e48cf3dbf8dfb48cea3b3b89333c3cca9d0
cyeoward <chris.yeoward@gmail.com>
2018-03-16 19:49:08 +0000

    Adds header config to Navigator requests

--- test/support/api.js
+++ test/support/api.js
@@ -8,13 +8,13 @@ export const onDiscover = (url, links) =>
         .addLinks(links)
         .toObject())
 
-export const onGet = (url, path, resource) =>
-  nock(url)
+export const onGet = (url, path, resource, headers) =>
+  nock(url, {reqheaders: headers})
     .get(path)
     .reply(200, resource.toObject())
 
-export const onPostRedirect = (url, path, body, location) =>
-  nock(url)
+export const onPostRedirect = (url, path, body, location, headers) =>
+  nock(url, {reqheaders: headers})
     .post(path, body)
     .reply(201, undefined, {
       Location: `${url}${location}`
