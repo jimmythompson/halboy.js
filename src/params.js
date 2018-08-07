966589db16ca034f4b8a9f40f75fb2329336f07d
Toby Clemson <tobyclemson@gmail.com>
2018-08-07 16:33:18 +0100

    Retain additional parameters as query parameters after URI templating.

--- src/params.js
+++ src/params.js
@@ -1,13 +1,22 @@
 import URI from 'urijs'
 import URITemplate from 'urijs/src/URITemplate'
+import { omit, merge, concat } from 'ramda'
 
 export const resolveLink = (templatedUrl, params) => {
-  const url = URITemplate(templatedUrl).expand(params)
+  const template = URITemplate(templatedUrl).parse()
+  const variables = template.parts.reduce((accumulator, part) => {
+    if (part.variables) {
+      return concat(part.variables.map(variable => variable.name), accumulator)
+    }
+    return accumulator
+  }, [])
+  const url = template.expand(params)
   const queryObject = URI(url).query(true)
+  const fullParams = merge(omit(variables, params), queryObject)
   const urlWithoutQueryString = URI(url).query('').toString()
 
   return {
     href: urlWithoutQueryString,
-    params: queryObject
+    params: fullParams
   }
 }
--- test/navigator/get.spec.js
+++ test/navigator/get.spec.js
@@ -161,6 +161,84 @@ describe('Navigator', () => {
     ])
   })
 
+  it('navigates with templated list query parameters', async () => {
+    api.onDiscover(baseUrl, {
+      users: {href: '/users{?ids*}', templated: true}
+    })
+
+    nock(baseUrl)
+      .get('/users')
+      .query({ids: ['123', '456']})
+      .reply(200,
+        new Resource()
+          .addResource('users', [
+            createUser({id: 'fred', name: 'Fred'}),
+            createUser({id: 'sue', name: 'Sue'}),
+            createUser({id: 'mary', name: 'Mary'})
+          ])
+          .toObject())
+
+    const discoveryResult = await Navigator.discover(baseUrl)
+    const result = await discoveryResult
+      .get('users', {ids: [123, 456]})
+
+    expect(result.status()).to.equal(200)
+
+    const users = result
+      .resource()
+      .getResource('users')
+
+    const names = users.map((user) =>
+      user.getProperty('name'))
+
+    expect(names).to.deep.equal([
+      'Fred',
+      'Sue',
+      'Mary'
+    ])
+  })
+
+  it('retains superfluous parameters after templating as query parameters',
+    async () => {
+      api.onDiscover(baseUrl, {
+        friends: {href: '/users/{id}/friends{?mutual}', templated: true}
+      })
+
+      nock(baseUrl)
+        .get('/users/thomas/friends')
+        .query({mutual: 'true', mostRecentlyContactedFirst: 'true'})
+        .reply(200,
+          new Resource()
+            .addResource('users', [
+              createUser({id: 'fred', name: 'Fred'}),
+              createUser({id: 'sue', name: 'Sue'}),
+              createUser({id: 'mary', name: 'Mary'})
+            ])
+            .toObject())
+
+      const discoveryResult = await Navigator.discover(baseUrl)
+      const result = await discoveryResult.get('friends', {
+        id: 'thomas',
+        mutual: 'true',
+        mostRecentlyContactedFirst: 'true'
+      })
+
+      expect(result.status()).to.equal(200)
+
+      const users = result
+        .resource()
+        .getResource('users')
+
+      const names = users.map((user) =>
+        user.getProperty('name'))
+
+      expect(names).to.deep.equal([
+        'Fred',
+        'Sue',
+        'Mary'
+      ])
+    })
+
   it('adds header options for navigation', async () => {
     api.onDiscover(baseUrl, {
       users: {href: '/users{?admin}', templated: true}
